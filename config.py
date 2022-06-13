75d25046f4bd88670400ff01d2e01dc751fe747d
EDM 1.15 <dev.edm115@gmail.com>
2022-06-13 16:30:12 +0200

    + API

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 class Config(object):
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", None))
