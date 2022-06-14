11b9b1ec3b4139ce7769b0da56a877d27894c2ac
EDM 1.15 <dev.edm115@gmail.com>
2022-06-14 16:25:48 +0200

    Variables here for editing

--- config.py
+++ config.py
@@ -5,3 +5,14 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", None))
+
+class Var(object):
+    currentuser = int()
+    idtodump = None
+    startrange = 1
+    stoprange = None
+    dumpid = int()
+    tagged = True
+    currentpost = int()
+    howmanyposts = int()
+    postlist = []
